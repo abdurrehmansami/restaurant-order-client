832333c276a6d3e3957a49ffbfecbde02f948848
abdurrehmansami <abdurrehmansherazi190@gmail.com>
2024-07-30 19:48:22 +0500

    added menu slider on menu page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,17 +3,23 @@ import { View, Text, FlatList, TouchableOpacity, StyleSheet, Button } from 'reac
 import axios from 'axios';
 import { Card, Title, Paragraph } from 'react-native-paper';
 import CartContext from '../contexts/CartContext';
-
+import Carousel from 'react-native-snap-carousel';
 const Menu = ({ navigation }) => {
   const [products, setProducts] = useState([]);
   const { cart } = useContext(CartContext);
+  const [categories,setCategories]= useState()
   console.log('CART IN MENU', cart);
   useEffect(() => {
     axios.get('http://192.168.18.143:3000/api/products')
-      .then(response => setProducts(response.data))
+      .then(response =>{ 
+        setProducts(response.data)
+        setCategories(response.data.map(prod=>prod.Category))
+        console.log((response.data.map(prod=>prod.Category)));
+      })
       .catch(error => console.log(error));
   }, []);
 
+  const filterProductsByCategory=()=>{}
   const renderItem = ({ item }) => (
     <TouchableOpacity onPress={() => navigation.navigate('Checkout', { product: item })}>
       <Card style={styles.card}>
@@ -31,6 +37,22 @@ const Menu = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.carouselContainer}>
+        <Carousel
+          data={categories}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              // style={[styles.categoryItem, selectedCategory === item.id && styles.selectedCategory]}
+              style={styles.categoryItem}
+              onPress={() => filterProductsByCategory(item.id)}
+            >
+             { item && <Text >{item.name}</Text>}
+            </TouchableOpacity>
+          )}
+          sliderWidth={400}
+          itemWidth={100}
+        />
+      </View>
       <FlatList
         data={products}
         keyExtractor={item => item.id.toString()}
@@ -62,6 +84,17 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderColor: '#ddd',
     alignItems: 'center',
+  },
+  carouselContainer: {
+    height: 100,
+    marginBottom: 20
+  },
+  categoryItem: {
+    backgroundColor: '#ddd',
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    alignItems: 'center'
   },
 });
 
