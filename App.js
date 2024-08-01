799b335648a167841776d04ac19bbb053530e635
Abdul Rehman <abdurrehmansherazi190@gmail.com>
2024-08-01 23:13:44 +0500

    corrected navigations based on bottom tab

--- App.js
+++ App.js
@@ -1,24 +1,82 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import Menu from './src/components/Menu';
+// import Checkout from './src/components/Checkout';
+// import Cart from './src/components/Cart';
+// import OrderForm from './src/components/OrderForm';
+// import { CartProvider } from './src/contexts/CartContext';
+
+// const Stack = createStackNavigator();
+
+// const App = () => {
+//   return (
+//     <CartProvider>
+//       <NavigationContainer>
+//         <Stack.Navigator /*initialRouteName="Menu"*/>
+//           <Stack.Screen name="Menu" component={Menu} />
+//           <Stack.Screen name="Checkout" component={Checkout} />
+//           <Stack.Screen name="Cart" component={Cart} />
+//           <Stack.Screen name="OrderForm" component={OrderForm} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </CartProvider>
+//   );
+// };
+
+// export default App;
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { Ionicons } from '@expo/vector-icons';
 import Menu from './src/components/Menu';
 import Checkout from './src/components/Checkout';
 import Cart from './src/components/Cart';
 import OrderForm from './src/components/OrderForm';
 import { CartProvider } from './src/contexts/CartContext';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MenuStack = () => (
+  <Stack.Navigator initialRouteName="Menu" screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Menu" component={Menu} />
+    <Stack.Screen name="Checkout" component={Checkout} />
+  </Stack.Navigator>
+);
+
+const CartStack = () => (
+  <Stack.Navigator initialRouteName="Menu" screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Cart" component={Cart} />
+    <Stack.Screen name="OrderForm" component={OrderForm} />
+  </Stack.Navigator>
+);
 
 const App = () => {
   return (
     <CartProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Menu">
-          <Stack.Screen name="Menu" component={Menu} />
-          <Stack.Screen name="Checkout" component={Checkout} />
-          <Stack.Screen name="Cart" component={Cart} />
-          <Stack.Screen name="OrderForm" component={OrderForm} />
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarActiveTintColor:'tomato',
+            tabBarInactiveTintColor: "gray",
+            // headerShown:false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === 'Home') {
+                iconName = focused ? 'cutlery' : 'cutlery';
+              } else if (route.name === 'Basket') {
+                iconName = focused ? 'shopping-cart' : 'list';
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
+          <Tab.Screen name="Home" component={MenuStack} />
+          <Tab.Screen name="Basket" component={CartStack} />
+        </Tab.Navigator>
       </NavigationContainer>
     </CartProvider>
   );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,36 +4,8 @@ import axios from 'axios';
 import { Card, Title, Paragraph } from 'react-native-paper';
 import CartContext from '../contexts/CartContext';
 import Carousel from 'react-native-snap-carousel';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import More from './More';
-const Tab = createBottomTabNavigator();
 
-function OrdersScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Orders!</Text>
-    </View>
-  );
-}
-
-function NotificationsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
-    </View>
-  );
-}
-
-function MoreScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>More!</Text>
-    </View>
-  );
-}
-const Menu = ({ navigation }) => {
+ const Menu = ({ navigation }) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const { cart } = useContext(CartContext);
@@ -52,11 +24,15 @@ const Menu = ({ navigation }) => {
           }
           return acc;
         }, {}));
-        console.log('unique',uniqueCategories);
         setCategories(uniqueCategories)
       })
       .catch(error => console.log(error));
   }, []);
+  useEffect(()=>{
+    if(cart.length==0){
+      setFilteredProducts(products)
+    }
+  },[cart])
   const filterProducts = (categoryId) => {
     if (categoryId === null) {
       setFilteredProducts(products); // Show all products if no category is selected
@@ -81,7 +57,6 @@ const Menu = ({ navigation }) => {
   const totalPrice = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
   return (
-    <>
     <View style={styles.container}>
       <View style={styles.carouselContainer}>
         <Carousel
@@ -110,36 +85,6 @@ const Menu = ({ navigation }) => {
         </TouchableOpacity>
       )}
     </View>
-    {/* <NavigationContainer independent={true}>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'cutlery' : 'cutlery';
-            } else if (route.name === 'Orders') {
-              iconName = focused ? 'list' : 'list';
-            } else if (route.name === 'Notifications') {
-              iconName = focused ? 'bell' : 'bell';
-            } else if (route.name === 'More') {
-              iconName = focused ? 'ellipsis-h' : 'ellipsis-h'
-            }
-
-            // You can return any component that you like here!
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'brown',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Orders" component={OrdersScreen} />
-        <Tab.Screen name="Notifications" component={NotificationsScreen}/>
-        <Tab.Screen name="More" component={More} />
-      </Tab.Navigator>
-    </NavigationContainer> */}
-    </>
   );
 };
 
